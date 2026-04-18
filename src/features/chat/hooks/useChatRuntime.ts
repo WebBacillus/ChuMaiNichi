80f86a56cb05e34b3d907d7b037b6874377e5aaf
J56164 <jjclub12099@gmail.com>
2026-04-18 19:00:24 +0700

    feat: chat runtime console.log test

--- src/features/chat/hooks/useChatRuntime.ts
+++ src/features/chat/hooks/useChatRuntime.ts
@@ -1,30 +1,43 @@
+import useAuthStore from "@/features/auth/stores/auth-store";
 import { useLocalRuntime, type ChatModelAdapter } from "@assistant-ui/react";
+import axios from "axios";
 
 export default function useChatRuntime() {
+  const { getAuthHeaders } = useAuthStore();
   const MyModelAdapter: ChatModelAdapter = {
     async run({ messages, abortSignal }) {
       // TODO replace with your own API
-      const result = await fetch("/api/chat", {
-        method: "POST",
+      const response = await axios.post("/api/chat", {
+        responseType: "stream",
         headers: {
+          ...getAuthHeaders,
           "Content-Type": "application/json",
         },
         // forward the messages in the chat to the API
         body: JSON.stringify({
-          messages,
+          messages: messages.map((m) => ({
+            role: m.role,
+            content:
+              typeof m.content === "string"
+                ? m.content
+                : m.content
+                    .filter((p) => p.type === "text")
+                    .map((p) => p.text)
+                    .join(""),
+          })),
         }),
         // if the user hits the "cancel" button or escape keyboard key, cancel the request
         signal: abortSignal,
       });
-      const data = await result.json();
-      return {
-        content: [
-          {
-            type: "text",
-            text: data.text,
-          },
-        ],
-      };
+
+      response.data.on("data", (chunk) => {
+        // logic to process stream data
+        console.log(chunk);
+      });
+
+      response.data.on("end", () => {
+        // logic for stream complete
+      });
     },
   };
 
