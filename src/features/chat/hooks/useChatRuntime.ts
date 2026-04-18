15e69d17c4d35d09e7e7d8847f42a3f8aeb32441
J56164 <jjclub12099@gmail.com>
2026-04-18 19:00:24 +0700

    feat: chat AI connection stub

--- src/features/chat/hooks/useChatRuntime.ts
+++ src/features/chat/hooks/useChatRuntime.ts
@@ -3,11 +3,25 @@ import { useLocalRuntime, type ChatModelAdapter } from "@assistant-ui/react";
 export default function useChatRuntime() {
   const MyModelAdapter: ChatModelAdapter = {
     async run({ messages, abortSignal }) {
+      // TODO replace with your own API
+      const result = await fetch("/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // forward the messages in the chat to the API
+        body: JSON.stringify({
+          messages,
+        }),
+        // if the user hits the "cancel" button or escape keyboard key, cancel the request
+        signal: abortSignal,
+      });
+      const data = await result.json();
       return {
         content: [
           {
             type: "text",
-            text: "Demo",
+            text: data.text,
           },
         ],
       };
